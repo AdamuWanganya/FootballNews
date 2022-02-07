c1c8f902cf453cb3b8c3f5def0979d721c330c06
AdamuWanganya <wanganyaadamu@gmail.com>
2022-02-07 10:16:01 +0300

    use bindview to locate views by id

--- app/src/main/java/com/moringa/footballnews/MainActivity.java
+++ app/src/main/java/com/moringa/footballnews/MainActivity.java
@@ -3,12 +3,25 @@ package com.moringa.footballnews;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.appNameTextView)
+    TextView appNameTextVIew;
+    @BindView(R.id.seachNewButton)
+    Button searchNewsButton;
+    @BindView(R.id.newsEditText)
+    EditText newsEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        searchNewsButton.setOnClickListener(this);
     }
 }
